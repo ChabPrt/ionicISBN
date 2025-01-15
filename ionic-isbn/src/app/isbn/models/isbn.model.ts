6cca53fc4d5581ed33d232a78599e670e69ba71c
ChabPrt <chabas.alexandre@gmail.com>
2025-01-15 23:36:20 +0100

    ✨Model : Isbn

---
+++ ionic-isbn/src/app/isbn/models/isbn.model.ts
@@ -0,0 +1,16 @@
+export type Isbn = {
+  code: string
+  bookMeta?: IsbnBookMeta
+}
+
+export type IsbnBookMeta ={
+  bib_key : string,
+  info_url: string
+  preview_url : string
+  thumbnail_url : string
+  details: {
+    title: string,
+    publishers : string[],
+    publish_date: string
+  }
+}
